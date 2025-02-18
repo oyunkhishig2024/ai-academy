3cf6da5b75f4b38be0c2a050dde3f41962b1f523
AI Academy Asia <ai3@192.168.1.98>
2025-02-18 20:06:39 +0800

    numpy uzew

---
+++ L8/ml.py
@@ -0,0 +1,19 @@
+# data prep then select ML algorithm
+#yaj tsugluulsan dat-gaa beldej tsegtsleh we gedeg n chuhal
+#niit data -g 2 huwaana niit ogogdol, shalgah ogogdol. 
+# data augmentation & synthetic data generation
+
+#teachablemachine
+
+#data evaluation phase, we use accuracy metric
+
+# whole process:
+# 1. data collection
+# 2. data cleaning/Prep
+# 3. Training
+# 4. Data evaluation
+
+import numpy as np
+random_arr = np.random.rand(2,3) # array with random values
+random_arr = np.random.rand(2,3)*100
+print(random_arr)
